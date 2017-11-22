48b357dac63a7e4e5c02235f803fd39efe455151
MrAmiD <nadymov2910@gmail.com>
2017-11-22 16:26:21 +0500

    prodCard.html

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+
 function MainSliderInit() {//слайдер на главной странице
     $('.main-slider').slick({
         prevArrow: '<button type="button" class="slick-prev sliderAboutArrow">' +
@@ -43,9 +44,43 @@ function changeCountNum(){//изменение кол-ва товаров в к�
         return false;
     });
 }
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+$(function() { 
 
 
-$(function() {
+
+    /*for tabs start*/
+    //    Default open Tabs
+    $("#defaultOpen1").click(function(){
+        $(this).addClass('active');
+    });
+    $("#defaultOpen1").click();
+
+
+
+
+    /*for tabs end*/
+
     // ===== Scroll to Top ====
     $(window).scroll(function() {
         if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
